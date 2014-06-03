c062d225cd1b1aba7fca5c593b685cff7933f595
Eric Trumble <schizo.eric@gmail.com>
2014-06-02 21:39:38 -0400

    Added Advance search controls to windows app

--- BlackLionTrader/BlackLionTrader.Windows/MainPage.xaml.cs
+++ BlackLionTrader/BlackLionTrader.Windows/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,6 +26,56 @@ namespace BlackLionTrader
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void ItemSearchBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox searchBox = (TextBox)sender;
+            if(searchBox.Text.Equals("Item Name"))
+            {
+                searchBox.Text = string.Empty;
+                searchBox.Foreground = new SolidColorBrush(Colors.Black);
+            }
+        }
+
+        private void LevelBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox lvlBox = (TextBox)sender;
+            if(lvlBox.Text.Equals("Min Lvl") || lvlBox.Text.Equals("Max Lvl"))
+            {
+                lvlBox.Text = string.Empty;
+                lvlBox.Foreground = new SolidColorBrush(Colors.Black);
+            }
+        }
+
+        private void ItemSearchBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox searchBox = (TextBox)sender;
+            if(searchBox.Text.Equals(""))
+            {
+                searchBox.Text = "Item Name";
+                searchBox.Foreground = new SolidColorBrush(Colors.Gray);
+            }
+        }
+
+        private void MinLevelBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox lvlBox = (TextBox)sender;
+            if(lvlBox.Text.Equals(""))
+            {
+                lvlBox.Text = "Min Lvl";
+                lvlBox.Foreground = new SolidColorBrush(Colors.Gray);
+            }
+        }
+
+        private void MaxLevelBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox lvlBox = (TextBox)sender;
+            if (lvlBox.Text.Equals(""))
+            {
+                lvlBox.Text = "Max Lvl";
+                lvlBox.Foreground = new SolidColorBrush(Colors.Gray);
+            }
         }
     }
 }
