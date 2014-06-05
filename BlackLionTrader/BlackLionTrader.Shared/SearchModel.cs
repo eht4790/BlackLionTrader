870ea404f748c1f0d7549283454c08415049e4bb
Eric Trumble <eht4790@rit.edu>
2014-06-04 21:21:11 -0400

    added properties for each Model type

--- BlackLionTrader/BlackLionTrader.Shared/SearchModel.cs
+++ BlackLionTrader/BlackLionTrader.Shared/SearchModel.cs
@@ -1,10 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Json;
 
 namespace BlackLionTrader
 {
     class SearchModel
     {
+        private HttpClient client = new HttpClient();
+
+        private List<string> types = new List<string>();
+
+        public List<string> Types
+        {
+            get { return types; }
+        }
+
+        private List<string> rarities = new List<string>();
+
+        public List<string> Rarities
+        {
+            get { return rarities; }
+        }
+        public SearchModel()
+        {
+            client.BaseAddress = new Uri("http://www.gw2spidy.com");
+            setup();
+        }
+
+        private void setup()
+        {
+            var result = client.PostAsync("api/v0.9/json/types", null).Result;
+            if (result.IsSuccessStatusCode)
+            {
+                string resultContent = result.Content.ReadAsStringAsync().Result;
+            }
+        }
     }
 }
