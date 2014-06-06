96bc27c0859850c5dd6b4b07361cf82298f3896f
Eric Trumble <eht4790@rit.edu>
2014-06-05 21:05:45 -0400

    Added controller for handling calls to the models

---
+++ BlackLionTrader/BlackLionTrader.Shared/Controller.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlackLionTrader
+{
+    class Controller
+    {
+        private SearchModel searchModel = new SearchModel();
+
+        public SearchModel SearchModel
+        {
+            get { return searchModel; }
+        }
+
+        private WatchModel watchModel = new WatchModel();
+
+        public WatchModel WatchModel
+        {
+            get { return watchModel; }
+        }
+
+        private GemsModel gemModel = new GemsModel();
+
+        public GemsModel GemModel
+        {
+            get { return gemModel; }
+        }
+
+        public Controller()
+        {
+
+        }
+    }
+}
