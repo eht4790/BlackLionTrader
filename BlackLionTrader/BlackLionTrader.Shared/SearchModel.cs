23386114d1c9d863470c1e967e1268840b3e8530
Eric Trumble <eht4790@rit.edu>
2014-06-08 12:28:38 -0400

    removed HttpClient and Json calls and moved it to JsonHelper

--- BlackLionTrader/BlackLionTrader.Shared/SearchModel.cs
+++ BlackLionTrader/BlackLionTrader.Shared/SearchModel.cs
@@ -1,16 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using Windows.Data.Json;
+
 
 namespace BlackLionTrader
 {
     class SearchModel
     {
-        private HttpClient client = new HttpClient();
-
         private List<string> types = new List<string>();
 
         public List<string> Types
@@ -26,17 +23,7 @@ namespace BlackLionTrader
         }
         public SearchModel()
         {
-            client.BaseAddress = new Uri("http://www.gw2spidy.com");
-            setup();
         }
 
-        private void setup()
-        {
-            var result = client.PostAsync("api/v0.9/json/types", null).Result;
-            if (result.IsSuccessStatusCode)
-            {
-                string resultContent = result.Content.ReadAsStringAsync().Result;
-            }
-        }
     }
 }
