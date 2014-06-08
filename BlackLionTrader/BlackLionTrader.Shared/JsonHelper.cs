9780100b1aa5acb8b10faac8841d882ffa0bc354
Eric Trumble <eht4790@rit.edu>
2014-06-08 12:31:34 -0400

    Handles all Json calls to gw2spidy and their results

---
+++ BlackLionTrader/BlackLionTrader.Shared/JsonHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace BlackLionTrader
+{
+    class JsonHelper
+    {
+        private HttpClient client = new HttpClient();
+
+        public JsonHelper()
+        {
+            client.BaseAddress = new Uri("http://www.gw2spidy.com");
+        }
+
+        public void getTypes()
+        {
+            var result = client.PostAsync("api/v0.9/json/types", null).Result;
+            if (result.IsSuccessStatusCode)
+            {
+                string resultContent = result.Content.ReadAsStringAsync().Result;
+            }
+        }
+
+    }
+}
