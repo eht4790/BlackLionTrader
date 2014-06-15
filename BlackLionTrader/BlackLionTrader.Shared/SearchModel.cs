d7f82d9e47f755d27acc23fcc459227659bc367e
Eric Trumble <eht4790@rit.edu>
2014-06-15 13:14:09 -0400

    Added functionality to update the state of advanced search options

--- BlackLionTrader/BlackLionTrader.Shared/SearchModel.cs
+++ BlackLionTrader/BlackLionTrader.Shared/SearchModel.cs
@@ -29,28 +29,107 @@ namespace BlackLionTrader
 {
     public class SearchModel
     {
-        private List<string> types = new List<string>();
-        private List<string> rarities = new List<string>();
+        private JsonHelper jsonHelper;
+        private List<Type> types = new List<Type>();
+        private List<Subtype> subtypes = new List<Subtype>();
+        private List<Rarity> rarities = new List<Rarity>();
+
+        private Type currentType = null;
+        private Subtype currentSubtype = null;
+        private Rarity currentRarity = null;
+
 
         /// <summary>
         /// A list of available types to filter search results
         /// </summary>
-        public List<string> Types
+        public List<Type> Types
         {
             get { return types; }
         }
 
         /// <summary>
+        /// A list of available subtypes of the selected type to filter search results
+        /// </summary>
+        public List<Subtype> SubTypes
+        {
+            get { return subtypes; }
+        }
+
+        /// <summary>
         /// A list of available rarity levels to filter search results
         /// </summary>
-        public List<string> Rarities
+        public List<Rarity> Rarities
         {
             get { return rarities; }
         }
 
-        public SearchModel()
+        public SearchModel(JsonHelper jsonHelper)
         {
+            this.jsonHelper = jsonHelper;
+            setup();
         }
 
+        /// <summary>
+        /// Initial setup of data members
+        /// </summary>
+        private void setup()
+        {
+            types = jsonHelper.getTypes();
+            rarities = jsonHelper.getRarities();
+        }
+
+        /// <summary>
+        /// Set currentType to the Type of the given id and updates the subtypes list.
+        /// If id equals -1 restore to default.
+        /// </summary>
+        /// <param name="id">The id of the new currently selected Type</param>
+        public void changeType(int id)
+        {
+            if (id == -1)
+            {
+                currentType = null;
+                currentSubtype = null;
+                subtypes = new List<Subtype>();
+            }
+            else
+            {
+                currentType = types[id];
+                subtypes = currentType.Subtypes;
+            }
+        }
+
+        /// <summary>
+        /// Set currentSubtype to the Subtype of the given id.
+        /// If id equals - 1 restore to default.
+        /// </summary>
+        /// <param name="id">The id of the new currently selected Subtype</param>
+        public void changeSubtype(int id)
+        {
+            if (id == -1)
+            {
+                currentSubtype = null;
+            }
+            else
+            {
+                currentSubtype = subtypes[id];
+            }
+        }
+
+        /// <summary>
+        /// Set currentRarity to the Rarity of the given id.
+        /// If id equals -1 restore to default.
+        /// </summary>
+        /// <param name="id"></param>
+        public void changeRarity(int id)
+        {
+            if(id == -1)
+            {
+                currentRarity = null;
+            }
+            else
+            {
+                currentRarity = rarities[id];
+            }
+        }
     }
 }
