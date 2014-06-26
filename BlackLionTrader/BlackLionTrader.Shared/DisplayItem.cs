231e9c723fb54e1e429a2c0808f72cc4b242f7f5
Eric Trumble <eht4790@rit.edu>
2014-06-25 21:49:39 -0400

    Added additional Properties for binding in the UI

--- BlackLionTrader/BlackLionTrader.Shared/DisplayItem.cs
+++ BlackLionTrader/BlackLionTrader.Shared/DisplayItem.cs
@@ -34,9 +34,28 @@ namespace BlackLionTrader
         private string type;
         private int supply;
         private int demand;
-        private int minSaleOffer;
-        private int maxBuyOffer;
-        private int margin;
+        private string minSaleOffer;
+        private string minSaleCopper;
+        private string minSaleCopperImg = null;
+        private string minSaleSilver;
+        private string minSaleSilverImg = null;
+        private string minSaleGold;
+        private string minSaleGoldImg = null;
+        private string maxBuyOffer;
+        private string maxBuyCopper;
+        private string maxBuyCopperImg = null;
+        private string maxBuySilver;
+        private string maxBuySilverImg = null;
+        private string maxBuyGold;
+        private string maxBuyGoldImg = null;
+        private string margin;
+        private int marginInt;
+        private string marginCopper;
+        private string marginCopperImg = null;
+        private string marginSilver;
+        private string marginSilverImg = null;
+        private string marginGold;
+        private string marginGoldImg = null;
 
         /// <summary>
         /// The url of the image associate with the file
@@ -98,26 +117,182 @@ namespace BlackLionTrader
         /// <summary>
         /// The lowest sell offer on the market
         /// </summary>
-        public int MinSaleOffer
+        public string MinSaleOffer
         {
             get { return minSaleOffer; }
         }
 
         /// <summary>
-        /// The highest buy offer on the market
+        /// The copper value of the lowest sell offer
         /// </summary>
-        public int MaxBuyOffer
+        public string MinSaleCopper
+        {
+            get { return minSaleCopper; }
+        }
+
+        /// <summary>
+        /// The source for copper coin image used in displaying the min sale price.
+        /// Null if image is uneeded.
+        /// </summary>
+        public string MinSaleCopperImg
+        {
+            get { return minSaleCopperImg; }
+        }
+
+        /// <summary>
+        /// The silver value of the lowest sell offer
+        /// </summary>
+        public string MinSaleSilver
+        {
+            get { return minSaleSilver; }
+        }
+
+        /// <summary>
+        /// The source for silver coin image used in displaying the min sale price.
+        /// Null if image is uneeded.
+        /// </summary>
+        public string MinSaleSilverImg
+        {
+            get { return minSaleSilverImg; }
+        }
+
+        /// <summary>
+        /// The gold value of the lowest sell offer
+        /// </summary>
+        public string MinSaleGold
+        {
+            get { return minSaleGold; }
+        }
+
+        /// <summary>
+        /// The source for gold coin image used in displaying the min sale price.
+        /// Null if image is uneeded.
+        /// </summary>
+        public string MinSaleGoldImg
+        {
+            get { return minSaleGoldImg; }
+        }
+
+        /// <summary>
+        /// The highest buy order on the market
+        /// </summary>
+        public string MaxBuyOffer
         {
             get { return maxBuyOffer; }
+        }
+
+        /// <summary>
+        /// The copper value of the highest buy order
+        /// </summary>
+        public string MaxBuyCopper
+        {
+            get { return maxBuyCopper; }
+        }
+
+        /// <summary>
+        /// The source for copper coin image used in displaying the max buy price.
+        /// Null if image is uneeded.
+        /// </summary>
+        public string MaxBuyCopperImg
+        {
+            get { return maxBuyCopperImg; }
+        }
+
+        /// <summary>
+        /// The silver value of the highest buy order
+        /// </summary>
+        public string MaxBuySilver
+        {
+            get { return maxBuySilver; }
+        }
+
+        /// <summary>
+        /// The source for silver coin image used in displaying the max buy price.
+        /// Null if image is uneeded.
+        /// </summary>
+        public string MaxBuySilverImg
+        {
+            get { return maxBuySilverImg;}
+        }
+
+        /// <summary>
+        /// The gold value of the highest buy order
+        /// </summary>
+        public string MaxBuyGold
+        {
+            get { return maxBuyGold; }
+        }
+
+        /// <summary>
+        /// The source for gold coin image used in displaying the max buy price.
+        /// Null if image is uneeded.
+        /// </summary>
+        public string MaxBuyGoldImg
+        {
+            get { return maxBuyGoldImg; }
         }
 
         /// <summary>
         /// The difference between the minSaleOffer and maxBuyOffer with
         /// the listing fee and selling fee included (15% tax)
         /// </summary>
-        public int Margin
+        public string Margin
         {
             get { return margin; }
+        }
+
+        /// <summary>
+        /// The copper value of the difference between the minSaleOffer with
+        /// the listing fee and selling fee included (15% tax)
+        /// </summary>
+        public string MarginCopper
+        {
+            get { return marginCopper; }
+        }
+
+        /// <summary>
+        /// The source for copper coin image used in displaying the margin.
+        /// Null if image is uneeded.
+        /// </summary>
+        public string MarginCopperImg
+        {
+            get { return marginCopperImg; }
+        }
+
+        /// <summary>
+        /// The silver value of the difference between the minSaleOffer with
+        /// the listing fee and selling fee included (15% tax)
+        /// </summary>
+        public string MarginSilver
+        {
+            get { return marginSilver; }
+        }
+
+        /// <summary>
+        /// The source for silver coin image used in displaying the margin.
+        /// Null if image is uneeded.
+        /// </summary>
+        public string MarginSilverImg
+        {
+            get { return marginSilverImg; }
+        }
+
+        /// <summary>
+        /// The gold value of the difference between the minSaleOffer with
+        /// the listing fee and selling fee included (15% tax)
+        /// </summary>
+        public string MarginGold
+        {
+            get { return marginGold; }
+        }
+
+        /// <summary>
+        /// The source for gold coin image used in displaying the margin.
+        /// Null if image is uneeded.
+        /// </summary>
+        public string MarginGoldImg
+        {
+            get { return marginGoldImg; }
         }
 
         public DisplayItem(string img,
@@ -131,15 +306,151 @@ namespace BlackLionTrader
                            int maxBuyOffer)
         {
             this.img = img;
-            this.name = name;
+            if(name.Length > 34)
+            {
+                this.name = name.Substring(0, 31) + "...";
+            }
+            else
+            {
+                this.name = name.PadRight(34);
+            }
             this.level = level;
             this.rarity = rarity;
             this.type = type;
             this.supply = supply;
             this.demand = demand;
-            this.minSaleOffer = minSaleOffer;
-            this.maxBuyOffer = maxBuyOffer;
-            this.margin = (int)(minSaleOffer * .85) - maxBuyOffer;
+            this.minSaleOffer = minSaleOffer.ToString();
+
+            // Divide up minSaleOffer price
+            if(this.minSaleOffer.Length < 3)
+            {
+                minSaleCopper = this.minSaleOffer;
+                minSaleSilver = "";
+                minSaleGold = "";
+                minSaleCopperImg = "SharedAssets/copper.png";
+            }
+            else if(this.minSaleOffer.Length < 4)
+            {
+                minSaleCopper = this.minSaleOffer.Substring(1);
+                minSaleSilver = " " + this.minSaleOffer.Substring(0,1);
+                minSaleGold = "";
+                minSaleCopperImg = "SharedAssets/copper.png";
+                minSaleSilverImg = "SharedAssets/silver.png";
+            }
+            else if(this.minSaleOffer.Length < 5)
+            {
+                minSaleCopper = this.minSaleOffer.Substring(2);
+                minSaleSilver = this.minSaleOffer.Substring(0, 2);
+                minSaleGold = "";
+                minSaleCopperImg = "SharedAssets/copper.png";
+                minSaleSilverImg = "SharedAssets/silver.png";
+            }
+            else if(this.minSaleOffer.Length >= 5)
+            {
+                int endGoldIndex = this.minSaleOffer.Length - 4;
+                minSaleCopper = this.minSaleOffer.Substring(endGoldIndex + 2);
+                minSaleSilver = this.minSaleOffer.Substring(endGoldIndex, 2);
+                minSaleGold = this.minSaleOffer.Substring(0, endGoldIndex);
+                minSaleCopperImg = "SharedAssets/copper.png";
+                minSaleSilverImg = "SharedAssets/silver.png";
+                minSaleGoldImg = "SharedAssets/gold.png";
+            }
+
+            // Divide up maxBuyOffer price
+            this.maxBuyOffer = maxBuyOffer.ToString();
+            if (this.maxBuyOffer.Length < 3)
+            {
+                maxBuyCopper = this.maxBuyOffer;
+                maxBuySilver = "";
+                maxBuyGold = "";
+                maxBuyCopperImg = "SharedAssets/copper.png";
+            }
+            else if (this.maxBuyOffer.Length < 4)
+            {
+                maxBuyCopper = this.maxBuyOffer.Substring(1);
+                maxBuySilver = " " + this.maxBuyOffer.Substring(0, 1);
+                maxBuyGold = "";
+                maxBuyCopperImg = "SharedAssets/copper.png";
+                maxBuySilverImg = "SharedAssets/silver.png";
+            }
+            else if (this.maxBuyOffer.Length < 5)
+            {
+                maxBuyCopper = this.maxBuyOffer.Substring(2);
+                maxBuySilver = this.maxBuyOffer.Substring(0, 2);
+                maxBuyGold = "";
+                maxBuyCopperImg = "SharedAssets/copper.png";
+                maxBuySilverImg = "SharedAssets/silver.png";
+            }
+            else if (this.maxBuyOffer.Length >= 5)
+            {
+                int endGoldIndex = this.maxBuyOffer.Length - 4;
+                maxBuyCopper = this.maxBuyOffer.Substring(endGoldIndex + 2);
+                maxBuySilver = this.maxBuyOffer.Substring(endGoldIndex, 2);
+                maxBuyGold = this.maxBuyOffer.Substring(0, endGoldIndex);
+                maxBuyCopperImg = "SharedAssets/copper.png";
+                maxBuySilverImg = "SharedAssets/silver.png";
+                maxBuyGoldImg = "SharedAssets/gold.png";
+            }
+
+            // Divide up margin price
+            this.marginInt = (int)(minSaleOffer * .85) - maxBuyOffer;
+            if (marginInt < 0)
+            {
+                this.margin = marginInt.ToString().Substring(1);
+            }
+            else
+            {
+                this.margin = marginInt.ToString();
+            }
+            if (this.margin.Length < 3)
+            {
+                marginCopper = this.margin;
+                marginSilver = "";
+                marginGold = "";
+                if(marginInt < 0)
+                {
+                    marginCopper = "-" + marginCopper;
+                }
+                marginCopperImg = "SharedAssets/copper.png";
+            }
+            else if (this.margin.Length < 4)
+            {
+                marginCopper = this.margin.Substring(1);
+                marginSilver = " " + this.margin.Substring(0, 1);
+                marginGold = "";
+                if(marginInt < 0)
+                {
+                    marginSilver = "-" + marginSilver;
+                }
+                marginCopperImg = "SharedAssets/copper.png";
+                marginSilverImg = "SharedAssets/silver.png";
+            }
+            else if (this.margin.Length < 5)
+            {
+                marginCopper = this.margin.Substring(2);
+                marginSilver = this.margin.Substring(0, 2);
+                marginGold = "";
+                if(marginInt < 0)
+                {
+                    marginSilver = "-" + marginSilver;
+                }
+                marginCopperImg = "SharedAssets/copper.png";
+                marginSilverImg = "SharedAssets/silver.png";
+            }
+            else if (this.margin.Length >= 5)
+            {
+                int endGoldIndex = this.margin.Length - 4;
+                marginCopper = this.margin.Substring(endGoldIndex + 2);
+                marginSilver = this.margin.Substring(endGoldIndex, 2);
+                marginGold = this.margin.Substring(0, endGoldIndex);
+                if(marginInt < 0)
+                {
+                    marginGold = "-" + marginGold;
+                }
+                marginCopperImg = "SharedAssets/copper.png";
+                marginSilverImg = "SharedAssets/silver.png";
+                marginGoldImg = "SharedAssets/gold.png";
+            }
         }
 
     }
